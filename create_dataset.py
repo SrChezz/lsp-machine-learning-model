0afad8d28a2dac6c6555b2d8b3666722f2b40a09
Chezzby <itzchezvil17@gmail.com>
2025-05-30 03:26:02 -0500

    fix: model correction

--- create_dataset.py
+++ create_dataset.py
@@ -1,50 +1,58 @@
 import os
 import pickle
-
 import mediapipe as mp
 import cv2
-import matplotlib.pyplot as plt
 
-
+# Inicializar MediaPipe Hands
 mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
+# Ruta donde están las carpetas con imágenes
 DATA_DIR = './data'
 
 data = []
 labels = []
-for dir_ in os.listdir(DATA_DIR):
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
-        data_aux = []
 
+for dir_ in os.listdir(DATA_DIR):
+    dir_path = os.path.join(DATA_DIR, dir_)
+    if not os.path.isdir(dir_path):
+        continue  # Ignorar archivos que no son carpetas
+
+    for img_path in os.listdir(dir_path):
+        data_aux = []
         x_ = []
         y_ = []
 
-        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img = cv2.imread(os.path.join(dir_path, img_path))
+        if img is None:
+            print(f"Imagen no válida: {img_path}")
+            continue
+
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
         results = hands.process(img_rgb)
+
         if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
+            # Procesar solo la primera mano detectada
+            hand_landmarks = results.multi_hand_landmarks[0]
 
-                    x_.append(x)
-                    y_.append(y)
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                x_.append(x)
+                y_.append(y)
 
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    data_aux.append(x - min(x_))
-                    data_aux.append(y - min(y_))
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
 
-            data.append(data_aux)
-            labels.append(dir_)
+            if len(data_aux) == 42:  # Validar que tiene el largo esperado
+                data.append(data_aux)
+                labels.append(dir_)
 
-f = open('data.pickle', 'wb')
-pickle.dump({'data': data, 'labels': labels}, f)
-f.close()
+# Guardar dataset en un archivo pickle
+with open('data.pickle', 'wb') as f:
+    pickle.dump({'data': data, 'labels': labels}, f)
+
+print(f'Dataset generado correctamente: {len(data)} muestras guardadas.')
